43d2b6fe318604b21cc239234db2ba529322c3da
Karlackerman-sophos <133684177+Karlackerman-sophos@users.noreply.github.com>
2025-07-28 08:12:28 -0400

    Update detection_common.hpp
    Set defaults to not_enabled, 1x day and monitor mode incase the loaded metadata for the detection was missing these fields.

--- detection_common.hpp
+++ detection_common.hpp
@@ -13,9 +13,9 @@ struct LoadedDetectionRule {
     std::string id;
     std::string name;
     std::string description;
-    bool enabled{ true };
-    int frequency_seconds{ 0 };
-    int monitor_mode{ 0 };
+    bool enabled{ false };
+    int frequency_seconds{ 86400 };
+    int monitor_mode{ 1 };
     std::string mitre_attack_mapping;
     int severity_score_default{ 0 };
     std::string sql_query_template;
